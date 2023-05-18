7ce4641b46c9d99f754afd90c125879a2a97a15c
isakurs <88219790+isakurs@users.noreply.github.com>
2023-05-18 08:45:04 -0400

    Fixed FindWord.py

--- FindWord.py
+++ FindWord.py
@@ -8,70 +8,20 @@ words = words.words()
 
 def Find():
     prompt = input("Find Word:").lower()
-    if prompt == "gay":
+    if prompt in words:
         print(f"{prompt} is in the wordlist")
         synsets = wordnet.synsets(prompt)
         if synsets:
             first = synsets[0]
             definition = first.definition()
             print(f"Definition:{definition}")
-            print("Example: Sean Brickmeier")
-            Find()
-        else:
-            print("No definition found")
-            Find()
-
-    elif prompt == "mistake":
-        print(f"{prompt} is in the wordlist")
-        synsets = wordnet.synsets(prompt)
-        if synsets:
-            first = synsets[0]
-            definition = first.definition()
-            print(f"Definition:{definition}")
-            print("Example: Nicholas Bonilla")
-            Find()
-        else:
-            print("No definition found")
-            Find()
-    elif prompt == "kitten":
-        print(f"{prompt} is in the wordlist")
-        synsets = wordnet.synsets(prompt)
-        if synsets:
-            first = synsets[0]
-            definition = first.definition()
-            print(f"Definition:{definition}")
-            print("Example: Isakur Stefansson")
-            Find()
-        else:
-            print("No definition found")
-            Find()
-    elif prompt == "poor":
-        print(f"{prompt} is in the wordlist")
-        synsets = wordnet.synsets(prompt)
-        if synsets:
-            first = synsets[0]
-            definition = first.definition()
-            print(f"Definition:{definition}")
-            print("Example: Victor (Too Poor For A Last Name)")
             Find()
         else:
             print("No definition found")
             Find()
     else:
-        if prompt in words:
-            print(f"{prompt} is in the wordlist")
-            synsets = wordnet.synsets(prompt)
-            if synsets:
-                first = synsets[0]
-                definition = first.definition()
-                print(f"Definition:{definition}")
-                Find()
-            else:
-                print("No definition found")
-                Find()
-        else:
-            print("Word not found")
-            Find()
+        print("Word not found")
+        Find()
 
 Find()
         
